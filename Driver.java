77b8a4b6227cfd6797cb33e54d19e856cf305aaa
Nurul Ain <72840762+ainSuhaimi@users.noreply.github.com>
2020-10-14 18:49:18 +0800

    Add files via upload

--- Driver.java
+++ Driver.java
@@ -5,10 +5,28 @@ public class Driver {
 		//Fish nemo; == int x;
 		Fish nemo = new Fish(); //nemo adalah object ,Fish adalah kelas
 		nemo.Swim();
-		nemo.color = "red"; //tukar value
+		//nemo.color = "red"; //tukar value
+		nemo.SetColor("red"); //change here
 		nemo.Swim();
 
-		Fish dory = new Fish(); // another object
-		dory.Swim();
+
+		//nemo.Eat(); //superclass call subclass method -not posibble
+
+
+		Shark fiery = new Shark();
+		fiery.Eat();
+		fiery.Swim();
+		fiery.SetSharkTeeth(130);
+		fiery.Eat();
+
+
+		// Fish dory = new Fish(); // another object
+		// dory.Swim();
+		// //dory.color = "red"; //tukar value
+		// dory.SetColor("yellow"); // change here
+		// dory.Swim();
+
+		// Aquirium fancyAquirium = new Aquirium();
+		// fancyAquirium.fillFish();
 	}
 }
--- Fish.java
+++ Fish.java
@@ -1,10 +1,21 @@
 public class Fish {
 	
-	//attribute or data 
-	public String color = "blue";
+	//attribute or data member or field
+	private String color = "blue";  //default value
 
-
+    //Method
 	public void Swim() {
 		System.out.println(color + " fish swim");
+	}
+
+	//setter /mutator method
+	public void SetColor(String warna) {
+		color = warna;
+
+	}
+	
+	//Getter / Accessor method
+	public String GetColor() {
+		return color;
 	}
 }
